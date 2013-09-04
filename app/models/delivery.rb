873ce570591c67571e5e6515cf25d5532d13960f
Michael Benedict <thebenedict@gmail.com>
2013-09-04 14:27:42 +0200

    annotated models

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: deliveries
+#
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  current_price   :integer
+#  amount_accepted :integer
+#  amount_rejected :integer
+#  comments        :string(255)
+#  created_at      :datetime
+#  updated_at      :datetime
+#
+
 class Delivery < ActiveRecord::Base
 end
--- app/models/transporter.rb
+++ app/models/transporter.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: transporters
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Transporter < ActiveRecord::Base
 end
