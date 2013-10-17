2b06fefac4d83db1dbc935d4f208d33496a15565
Connor Leech <connorleech@gmail.com>
2013-10-17 16:40:55 +0200

    delivery validation

---
+++ app/assets/javascripts/deliveries.js
@@ -0,0 +1,52 @@
+/*Place all the behaviors and hooks related to the matching controller here.
+# All this logic will automatically be available in application.js.
+# You can use CoffeeScript in this file: http://coffeescript.org/
+*/
+
+$(document).on("ready page:load", function(){
+	var form = $('#formCol form');
+	
+	//price
+	var currPrice = $('#currPrice'),
+		priceContain = $('#priceContain'),
+		priceMsg = $('#priceMsg');
+	
+	console.log(currPrice, priceMsg, priceContain);
+	
+	//accepted
+	var accepted = $('#accepted'),
+		acceptedContain = $('#acceptedContain'),
+		acceptedMsg = $('#acceptMsg');
+	
+	//rejected
+	var rejected = $('#rejected'),
+		rejectedContain = $('#rejectedContain'),
+		rejectedMsg = $('#rejectMsg');
+	
+	$('.help-inline').css('visibility', 'hidden');	//hide messages
+	
+	var validate = function(input, contain, msg){
+		var error = "This field must be a number";
+		
+		if(isNaN(input.val()) || input.val() == "" ){
+			//not a number or is empty string
+			contain.removeClass('has-success').addClass('has-error');
+			msg.text(error).css({visibility: 'visible'}).addClass("text-danger");
+			return false;
+		} else {
+			return true;
+		}
+	};
+
+	
+	form.on('submit', function(e){
+		
+		var p = validate(currPrice, priceContain, priceMsg);
+		var a = validate(accepted, acceptedContain, acceptedMsg);
+		var r = validate(rejected, rejectedContain, rejectedMsg);
+		
+		if(!p || !a || !r){		//does not pass validations
+			e.preventDefault();
+			}
+		});
+})
