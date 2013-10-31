3ea63fe172a4e6629907e55fbe5051265541199a
Connor Leech <connorleech@gmail.com>
2013-10-31 13:03:39 +0200

    all JS in application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,12 +16,21 @@
 //= require twitter/bootstrap
 //= require_tree .
 
+
+
 $(document).on("ready page:load", function(){
 	
 	$('.help-inline').css('visibility', 'hidden');	//hide messages
 	
 	var form = $('#formCol form');
 	
+	if (form.html()){
+		var id = form.attr('id').substr(0,4); //get form id if there's a form
+	}
+		
+	if (id === "tran"){
+	
+	//TRANSPORTER VALIDATION
 	//name validation
 	var nameField = $('#nameField');
 	var validateName = function(){
@@ -160,7 +169,61 @@ $(document).on("ready page:load", function(){
 			console.log("does not pass");
 		}
 	}); 
-})
+} //end transporters if
+
+else if (id == "deli") 
+
+{
+//DELIVERIES VALIDATION
+	
+	//price
+	var currPrice = $('#currPrice'),
+		priceContain = $('#priceContain'),
+		priceMsg = $('#priceMsg');
+		
+	//accepted
+	var accepted = $('#accepted'),
+		acceptedContain = $('#acceptedContain'),
+		acceptedMsg = $('#acceptMsg');
+	
+	//rejected
+	var rejected = $('#rejected'),
+		rejectedContain = $('#rejectedContain'),
+		rejectedMsg = $('#rejectMsg');
+	
+	$('.help-inline').css('visibility', 'hidden');	//hide messages
+	
+	var validate = function(input, contain, msg){
+		var error = "This field must be a number";
+		
+		if (input.val() == ""){
+			//empty field becomes 0
+			input.val(0);
+			}
+			
+		if(isNaN(input.val())){
+			//not a number or is empty string
+			contain.removeClass('has-success').addClass('has-error');
+			msg.text(error).css({visibility: 'visible'}).addClass("text-danger");
+			return false;
+		} else {
+			return true;
+		}
+	};
+
+	
+	form.on('submit', function(e){
+		
+		var p = validate(currPrice, priceContain, priceMsg);
+		var a = validate(accepted, acceptedContain, acceptedMsg);
+		var r = validate(rejected, rejectedContain, rejectedMsg);
+		
+		if(!p || !a || !r){		//does not pass validations
+			e.preventDefault();
+			}
+		});
+	}
+});
 
 	
 function testRegEx(value, filter){
